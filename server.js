4a538d8fd31c37f0ef5ea281a5167d41a2f6ca4b
Miles <milesmin805@gmail.com>
2026-06-04 23:26:13 -0700

    Fix FFmpeg preset syntax for fluent-ffmpeg

--- server.js
+++ server.js
@@ -848,7 +848,6 @@ app.post("/uploadClip", upload.single("clip"), (req, res) => {
     .audioCodec('aac')
     .videoBitrate('2500k')
     .audioBitrate('128k')
-    .option('-preset fast')
     .videoFilters(drawtextFilter)
     .output(mp4Path)
     .on("end", () => {
